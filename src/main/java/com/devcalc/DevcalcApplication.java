f77e146db0a4b4c3cea464c97a74f1ad991c8ad8
taohansens <taohansens@gmail.com>
2025-05-01 16:02:12 -0300

    [INIT] Created Project

---
+++ src/main/java/com/devcalc/DevcalcApplication.java
@@ -0,0 +1,13 @@
+package com.devcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevcalcApplication.class, args);
+    }
+
+}
