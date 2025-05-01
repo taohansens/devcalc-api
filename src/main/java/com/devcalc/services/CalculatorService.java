15c824260b996f64396bfc220f125a29504c6b04
taohansens <taohansens@gmail.com>
2025-05-01 16:41:31 -0300

    [FEAT] Controller/Service. Adds Junit. Update Readme.

---
+++ src/main/java/com/devcalc/services/CalculatorService.java
@@ -0,0 +1,26 @@
+package com.devcalc.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Não é possível dividir por zero");
+        }
+        return a / b;
+    }
+}
